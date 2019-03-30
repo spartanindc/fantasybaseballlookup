fff2e66d2070107d010d0b92b4aafa0d14ab18e4
David Alim <daveay@gmail.com>
2019-03-30 18:09:55 -0400

    improvements mk I

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Fantasy Baseball Player Lookup
 
-## Link to App
-[spartanindc.github.io/fantasybaseballlookup](https://spartanindc.github.io/fantasybaseballlookup)
-
-![FBSL Landing Page](https://github.com/spartanindc/fantasybaseballlookup/blob/master/FBSL_landing.png)
-![FBSL Results Page](https://github.com/spartanindc/fantasybaseballlookup/blob/master/FBSL_results.png)
-
 ## Summary
 
-The Fantasy Baseball Player Lookup app, pulls stats important to fantasy baseball players, allowing them to quickly find the best players for their chosen stat at each position.  The user selects the position they want to find stats for, and depending on the choice, hitting or pitching stat options are displayed to chose from.  The user then submits the search and results are given in a table.
+The Fantasy Baseball Player Lookup app, pulls stats important to fantasy baseball players, allowing them to quickly find the best players for their chosen stat at each position. The user selects the position they want to find stats for, and depending on the choice, hitting or pitching stat options are displayed to chose from. The user then submits the search and results are given in a table.
+
+## Link to App
+
+[spartanindc.github.io/fantasybaseballlookup](https://spartanindc.github.io/fantasybaseballlookup)
+
+![FBSL Landing Page](https://github.com/spartanindc/fantasybaseballlookup/blob/master/FBSL_landing.png)
+![FBSL Results Page](https://github.com/spartanindc/fantasybaseballlookup/blob/master/FBSL_results.png)
 
 ## Technology used
 
-* Javascript
-* HTML
-* CSS
-* Jquery
+- Javascript
+- HTML
+- CSS
+- Jquery
--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 //Variables
 
-const MLB_STATS_URL = "https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/cumulative_player_stats.json?";
+const MLB_STATS_URL =
+  "https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/cumulative_player_stats.json?";
 
-const statTrans = new Map([["R", "Runs"],
-                           ["RBI", "RunsBattedIn"],
-                           ["HR", "Homeruns"],
-                           ["SB", "StolenBases"],
-                           ["AVG", "BattingAvg"],
-                           ["ERA", "EarnedRunAvg"],
-                           ["W", "Wins"],
-                           ["SV", "Saves"],
-                           ["WHIP", "WalksAndHitsPerInningPitched"],
-                           ["SO", "PitcherStrikeouts"]]);
+const statTrans = new Map([
+  ["R", "Runs"],
+  ["RBI", "RunsBattedIn"],
+  ["HR", "Homeruns"],
+  ["SB", "StolenBases"],
+  ["AVG", "BattingAvg"],
+  ["ERA", "EarnedRunAvg"],
+  ["W", "Wins"],
+  ["SV", "Saves"],
+  ["WHIP", "WalksAndHitsPerInningPitched"],
+  ["SO", "PitcherStrikeouts"]
+]);
 
 //Landing Page
 
 function letsGoButtonClicked() {
-	$('.startButton').on('click', event => {
-		event.preventDefault();
-		displayTools();
-		dynamicForm();	
-	});
+  $(".startButton").on("click", event => {
+    event.preventDefault();
+    displayTools();
+    dynamicForm();
+  });
 }
 
 //Main Page Render & Display
 
 function resetResults() {
-	$('.results').html(`
-		<h2>Results</h2>	
+  $(".results").html(`
+		<h2>Results</h2>
 		<table id='resultTable'>
 				<tr>
 					<th>Name</th>
@@ -37,123 +40,138 @@ function resetResults() {
 }
 
 function submitButtonClicked() {
-	$('.submitButton').on('click', event => {
-		event.preventDefault();
-		$('.results').removeAttr('hidden');
-		resetResults();
-		getMLBData();	
-		$('.results').show();
-	});
+  $(".submitButton").on("click", event => {
+    event.preventDefault();
+    if ($("#pos").val() === "") {
+      alert("Must pick a position!");
+      console.log("No position picked");
+    } else if ($("#pos").val() != "") {
+      $(".results").removeAttr("hidden");
+      resetResults();
+      getMLBData();
+      $(".results").show();
+    }
+  });
 }
 
 function dynamicForm() {
-	$('#hittingSelect').hide();
-	$('#pitchingSelect').hide();
-	$('#pos').on('change', event => {
-		if ($('#pos').val() === 'P') {
-			$('#pitchingSelect').show();
-			$('#hittingSelect').hide();
-		} else if ($('#pos').val() != 'P') {
-		$('#hittingSelect').show();
-		$('#pitchingSelect').hide();
-		}
-	});
+  $("#hittingSelect").hide();
+  $("#pitchingSelect").hide();
+  $("#pos").on("change", event => {
+    if ($("#pos").val() === "P") {
+      $("#pitchingSelect").show();
+      $("#hittingSelect").hide();
+    } else if ($("#pos").val() != "P") {
+      $("#hittingSelect").show();
+      $("#pitchingSelect").hide();
+    }
+  });
 }
 
 function displayTools() {
-	$('.startText').remove();
-	renderStatsPage();
-	submitButtonClicked();
-} 
-
+  $(".startText").remove();
+  renderStatsPage();
+  submitButtonClicked();
+}
 
 function displayResults(data) {
-	
-	let statAbbr = $('#pit').val(); 
-		if ($('#pos').val() != 'P') {
-			statAbbr = $('#hit').val();
-		}
-	let statName = statTrans.get(statAbbr);
-	let players = data.cumulativeplayerstats.playerstatsentry;
-	
-	let statTag = "";
-		if (statAbbr == "SO") {
-			statTag = "K";
-		} else {
-			statTag = statAbbr;
-		}
-	
-	let tableResults = "";
-			
-	for (i=0; i < players.length; i++) {
-		player = players[i].player;
-		tableResults += "<tr>" +
-						"<td>" + player.FirstName + " " + player.LastName + "</td>" +
-						"<td>" + players[i].stats[statName]["#text"] + " " + statTag + "</td>" +
-						"</tr>";
-	}
-	
-	$('#resultTable').append(tableResults);
+  let statAbbr = $("#pit").val();
+  if ($("#pos").val() != "P") {
+    statAbbr = $("#hit").val();
+  }
+  let statName = statTrans.get(statAbbr);
+  let players = data.cumulativeplayerstats.playerstatsentry;
+
+  let statTag = "";
+  if (statAbbr == "SO") {
+    statTag = "K";
+  } else {
+    statTag = statAbbr;
+  }
+
+  let tableResults = "";
+
+  for (i = 0; i < players.length; i++) {
+    player = players[i].player;
+    tableResults +=
+      "<tr>" +
+      "<td>" +
+      player.FirstName +
+      " " +
+      player.LastName +
+      "</td>" +
+      "<td>" +
+      players[i].stats[statName]["#text"] +
+      " " +
+      statTag +
+      "</td>" +
+      "</tr>";
+  }
+
+  $("#resultTable").append(tableResults);
 }
 
 function renderStatsPage() {
-	$('#inputOutput').removeAttr('hidden');
-	$('.results').hide();
+  $("#inputOutput").removeAttr("hidden");
+  $(".results").hide();
 }
 
 //API Call
 
 function getMLBData() {
-	
-	let statAbbr = $('#pit').val(); 
-		if ($('#pos').val() != 'P') {
-			statAbbr = $('#hit').val();
-		}
-	
-	let sortDecider = "";
-		if ($('#pos').val() == 'P' && $('#pit').val() == 'ERA' || $('#pos').val() == 'P' && $('#pit').val() == 'WHIP') {
-			sortDecider = ".A";
-			} else {
-				sortDecider = ".D";
-                }
-	
-	let playerPosition = "";
-		if ($('#pos').val() == 'OF') {
-			playerPosition = 'OF,LF,CF,RF';
-		} else {
-			playerPosition = $('#pos').val();
-		}
-	
-	let sortSettings = "";
-		if (statAbbr == "SO") {
-			sortSettings = "stats.Pitching-SO.D";
-		} else {
-			sortSettings = "stats." + statAbbr + sortDecider;
-		}
-	
-	let settings = {
-	type: 'GET',
-	headers: {
-		"Authorization": 'Basic c3BhcnRhbmluZGM6RnRCMDUyMDAy'
-		},
-	url: MLB_STATS_URL,
-	dataType: 'json',
-	data: {
-		position: playerPosition,
-		playerstats: statAbbr,
-		limit: 20,
-		sort: sortSettings,
-		},
-	crossDomain: true,
-	success: displayResults,
-	error: displayError,
-	};
-		
-	$.ajax(settings);
+  let statAbbr = $("#pit").val();
+  if ($("#pos").val() != "P") {
+    statAbbr = $("#hit").val();
+  }
+
+  let sortDecider = "";
+  if (
+    ($("#pos").val() == "P" && $("#pit").val() == "ERA") ||
+    ($("#pos").val() == "P" && $("#pit").val() == "WHIP")
+  ) {
+    sortDecider = ".A";
+  } else {
+    sortDecider = ".D";
+  }
+
+  let playerPosition = "";
+  if ($("#pos").val() == "OF") {
+    playerPosition = "OF,LF,CF,RF";
+  } else {
+    playerPosition = $("#pos").val();
+  }
+
+  let sortSettings = "";
+  if (statAbbr == "SO") {
+    sortSettings = "stats.Pitching-SO.D";
+  } else {
+    sortSettings = "stats." + statAbbr + sortDecider;
+  }
+
+  let settings = {
+    type: "GET",
+    headers: {
+      Authorization: "Basic c3BhcnRhbmluZGM6RnRCMDUyMDAy"
+    },
+    url: MLB_STATS_URL,
+    dataType: "json",
+    data: {
+      position: playerPosition,
+      playerstats: statAbbr,
+      limit: 20,
+      sort: sortSettings
+    },
+    crossDomain: true,
+    Access-Control-Allow-Origin: *,
+    success: displayResults,
+    error: displayError
+  };
+
+  $.ajax(settings);
 }
 
 function displayError(data) {
-	console.log('Something went wrong');
+  console.log("Something went wrong");
 }
 
 //Run the page
